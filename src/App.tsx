c2a87e64f4baa1b983a7fb0e9e13f9fe5413f4ff
thijsvndmeer <thijsvandermeer@outlook.com>
2025-10-19 21:07:11 +0200

    Revert "deliberate error"
    This reverts commit fc15f98212464fa5c44392b8e393cf7ad6188149.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import SharedFavorites from "./pages/SharedFavorites";
 import Random from "./pages/Random";
 import { Header } from "./components/Header";
 import { useState, useEffect } from "react";
-import ErrorThrower from './components/ErrorThrower';
 
 const queryClient = new QueryClient();
 
@@ -67,7 +66,6 @@ export const AppContent = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <ErrorThrower />
       <Toaster />
       <Sonner />
       <div className="min-h-screen bg-gradient-subtle">
@@ -78,3 +76,5 @@ const App = () => (
 );
 
 export default App;
+
+
