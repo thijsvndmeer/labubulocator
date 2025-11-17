64bbee1f326694255e292749ef72bafdc088a21a
thijsvndmeer <thijsvandermeer@outlook.com>
2025-11-17 18:16:24 +0100

    sus

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -55,6 +55,8 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin && allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
   next();
 });
