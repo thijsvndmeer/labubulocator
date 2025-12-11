5a95ddc781d556eeabe8231ec1fa8e1613b142e5
thijsvndmeer <thijsvandermeer@outlook.com>
2025-12-11 16:52:51 +0100

    github pages fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,16 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
