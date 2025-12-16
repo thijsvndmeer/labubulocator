39e97eeb85db91cf6ed246bf25444fb28209d8db
thijsvndmeer <thijsvndmeer@users.noreply.github.com>
2025-12-16 17:52:06 +0000

    deploy: ddbe32b4feae655948c41a6408f6c500de97f905

--- assets/arrow-left-CvngvUOP.js
+++ assets/arrow-left-CvngvUOP.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BHWYs0VF.js";/**
+import{c as e}from"./index-KSySvc3j.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/share-2-BriRuUxg.js
+++ assets/share-2-BriRuUxg.js
@@ -1,4 +1,4 @@
-import{c}from"./index-BHWYs0VF.js";/**
+import{c}from"./index-KSySvc3j.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
